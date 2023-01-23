8276760426c3d6cdebb18a976c6f5fc822f94d4c
Mirabbosbek9778 <jwicj969@gmail.com>
2023-01-23 11:35:59 +0500

    test

---
+++ index.js
@@ -0,0 +1,99 @@
+// console.log(22);
+// let text;
+
+// console.log(text);
+
+// text='webbrain academy'
+
+// console.log(text);
+
+// let num = 1,
+//   a = 2,
+//   b = 3;
+
+//   console.log(num,a,b);
+
+// let a = (b = c = 3);
+
+// console.log(a,b,c);
+
+// let name="Mirabbos";
+
+// console.log("Abduvohidov " +name);
+
+// let name = "Mirabbosbek";
+
+// console.log(`Hello ${name} Webbrain Academy`);
+
+// let name = 'Mirabbosbek';
+
+// console.log(typeof name);
+
+// let a;
+// let b=null;
+
+// console.log(typeof a,typeof b);
+
+// let a;
+// let b=null;
+
+// console.log(null==undefined);
+
+// let a;
+// console.log(null+2+3);
+// console.log(undefined+2+3);///mavjud bomagan narsa
+
+// let a;
+// let b=null;
+
+// console.log(a+1);
+// console.log(b+1);
+// console.log({b}==={b});
+// console.log(b===b);
+
+// let a=Symbol("text");
+// let b=Symbol("text");
+
+// console.log(b==a);
+// console.log(b===a);
+
+// let ar = ["mirabbos", 1, 2, 3, "javohir"];
+// let obj = { name: "webbrain", status: "IT Center" };
+// let func=() =>{};
+// function test(){
+//     console.log(ar[0]);
+//     console.log(obj.name);
+// }
+// test(\
+/////////////////////////////////
+
+// let a = 1;
+// let b = "1";
+
+// let str1 = "Mirabbosbek";
+// let str2 = "Abduvohidov";
+
+// // console.log(str1 + str2);
+// // console.log(a + b);
+
+// ///1+"0  1"=11
+// ///+"1"
+// console.log(b);
+
+// let a = "1";
+// let b = 1;
+// let str1 = "1a";
+
+// console.log(isNaN(str1));
+// console.log(b + +a);
+// console.log(true == 1);
+// console.log(false == 0);
+// console.log(true == "1");
+// console.log(null == false);
+// console.log(undefined == false);
+// console.log(null == undefined);
+
+// let a = 1;
+// let b = 10;
+
+// console.log(a / +b);
