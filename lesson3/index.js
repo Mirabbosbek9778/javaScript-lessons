7e58558d6f2d311d7d142031714635bb903a735f
Mirabbosbek9778 <jwicj969@gmail.com>
2023-01-24 12:25:43 +0500

    if else, ternary

---
+++ lesson3/index.js
@@ -0,0 +1,76 @@
+// let temp = undefined;
+
+// if (temp) {
+//   console.log("svet o'chiq");
+// }
+
+// if (true) {
+//   console.log("svet yoniq");
+// };
+
+// if (age > 18) {
+//   console.log("you are under 18 years of age!");
+// }
+
+// let a = 5;
+// let b = 2;
+
+// if (a < b) {
+//   console.log(true);
+// } else {
+//   console.log(false);
+// }
+// let a = 1;
+// let b = 3;
+// let c = 5;
+
+// if(a<b && b<c){
+//     console.log("togri");
+// }else{
+//     console.log("hato");
+// }
+
+// let temp = "bulutli";
+
+// if(temp==="quyoshli"){
+//     console.log("havo quyoshli, konditsionerni yo'qing");
+// }else if(temp==="yomgirli"){
+//     console.log("havo yomgirli, soyabon oling");
+// }else if(temp==="bulutli"){
+//     console.log("havo bulutli, yomgir yogishi kutulmoqda!");
+// }else{
+//     console.log("no'malum ob havo!");
+// }
+
+// let a = 5;
+// if (a % 2 == 0) {
+//   console.log("juft son");
+// } else {
+//   console.log("toq son");
+// }
+
+
+// if (temp < 0) {
+    //   console.log("havo juda sovuq");
+    // } else if (temp < 10) {
+        //   console.log("havo ortacha boladi");
+        // } else if (temp <= 15) {
+            //   console.log("havo isiydi");
+            // } else if (temp <= 25) {
+                //   console.log("havo juda ham issiq");
+                // }
+                
+                // temp < 0 ? console.log("havo juda sovuq") : console.log("not found");
+                
+               
+//  let temp = 50;
+//  temp < 0
+//  ? console.log("havo juda sovuq") 
+//  :temp<=10 
+//  ? console.log("havo sovidi")
+//  :temp<=15 
+//  ? console.log("havo sovidi")
+//  :temp<=25 
+//  ? console.log("havo sovidi")
+//  : console.log("not found");   
+console.log(2*pow(2));
