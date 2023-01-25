440879982d2cf1bf4edddde4519e286f5cda7299
Mirabbosbek9778 <jwicj969@gmail.com>
2023-01-25 18:24:59 +0500

    Switch case, Forloop ,Nestedloop

--- lesson4/index.js
+++ lesson4/index.js
@@ -1,5 +1,53 @@
-var sum = 0;
-for (i = 0; i < 12; i++) {
-  console.log((sum += i));
-  if (sum == 10) break;
-}
+/////////sumning qiymati
+// var sum = 0;
+// for (i = 1; i < 12; i++) {
+//   //   console.log((sum += i));
+//   console.log(`sumni qiymati = ${(sum += i)}`);
+//   //   if (sum === 10) break;    // 1 3 6 10
+//   //   if (i === 1) break;   // 1
+//   if (sum === 10) break;
+// }
+
+/////// yigindisini topish va  juft toqqa tekshirish
+
+// let i = 1;
+// let sum = 0;
+// for (i = 1; i <= 5; i++) {
+//   sum += i;
+// //   console.log(sum);
+//   if (sum%2==0) {
+//     console.log(sum);
+//   }
+// }
+
+//// sonni juft va toq sonlar
+
+// let i = 1;
+// for (i = 1; i <=5; i++) {
+//   if (i % 2 == 0) {
+//     console.log(`juft  son : ${i}`);
+//   }else{
+//     console.log(`toq son : ${i}`);
+//   }
+// }
+
+//// sumning qiymatini ichidagi jarayonlar
+
+// let i = 1;
+// let sum = 0;
+// for (i = 1; i <= 5; i++) {
+//   console.log(`sumni qiymati : ${sum} +${i}= ${(sum+=i)}`);
+//   if(sum===10)
+// }
+
+// console.log(i++);
+// console.log(++i);
+// ++i
+// i++
+// i+=2
+// ++i++
+
+// for (let i = 1; i < 12; i++) {
+//   console.log(i);
+//   i += i;
+// }
