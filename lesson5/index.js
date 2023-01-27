09f3c97af7a226e41079f6aa00d0f97246983719
Mirabbosbek9778 <jwicj969@gmail.com>
2023-01-27 23:28:25 +0500

    while,do while, function

---
+++ lesson5/index.js
@@ -0,0 +1,53 @@
+////// while loop
+// let a = Math.round(Math.random() * 100);
+// let a=28;
+// let sum = 0;
+// let c = 0;
+
+// while (sum<a) {
+//   c++;
+//   sum+=c;
+// }
+// console.log(c);
+///1+2+3+4+5+6+7
+// if (a == sum) {
+//   console.log(`3ga  karrali sonlar`);
+// } else {
+//   console.log(`3ga karrai emas`);
+// }
+
+///////////////////////
+
+// let num = 10;
+// let karra=3;
+// for (let i = 1; i <= num; i++) {
+// if(i%karra==0){
+//     console.log(i);
+// }
+// }
+
+///////////////////////////
+
+// let i = 3;
+
+// // while (0) {
+// //   console.log(`hello world`);
+// // }
+// do {
+//   console.log(`hello world`);
+//   i--;
+// } while (i == 0);
+
+////////////////////
+
+function getAvr() {
+  console.log(`Mirabbos`);
+  console.log(`Mirabbos`);
+  console.log(`Mirabbos`);
+  console.log(`Mirabbos`);
+  console.log(`Mirabbos`);
+  console.log(`Mirabbos`);
+  console.log(`--------------------------------`);
+}
+// getAvr();
+// getAvr();
