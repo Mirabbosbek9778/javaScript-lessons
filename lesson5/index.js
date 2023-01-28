57d0be61d87c02abb43b6832bce9699ea69925c3
Mirabbosbek9778 <jwicj969@gmail.com>
2023-01-28 16:57:20 +0500

    while,do while, function

--- lesson5/index.js
+++ lesson5/index.js
@@ -76,11 +76,110 @@
 
 // let name='mirabbos'
 
-const getAvr = function (name) {
-  // console.log(name);
-  console.log(`${name} , Mirabbosbek Abduvohidov study in Webbrain Academy`);
-};
-getAvr("hello world"); //function col deydi
-getAvr("hello world"); //function col deydi
-getAvr("hello world"); //function col deydi
-getAvr("hello world"); //function col deydi
+// const getAvr = function (name) {
+//   // console.log(name);
+//   console.log(`${name} , Mirabbosbek Abduvohidov study in Webbrain Academy`);
+// };
+// getAvr("hello world"); //function col deydi
+// getAvr("hello world"); //function col deydi
+// getAvr("hello world"); //function col deydi
+// getAvr("hello world"); //function col deydi
+
+//////////////////
+
+// const getAvr = function (name, surname, age) {
+//   // console.log(name);
+//   console.log(`name : ${name}`);
+//   console.log(`Surname : ${surname}`);
+//   console.log(`Age : ${age} `);
+//   console.log(`==============================================`);
+// };
+// getAvr("Mirabbos", "Abduvohidov", 20);
+// getAvr("JAvohir", "Turgunpolatov", 20);
+// getAvr("Mirjalol", "Abduvohidov", 24);
+// getAvr("YAxyobek", "Abduvohidov", 26);
+
+// ///////////////////
+// let name = "Mirabbos";
+
+// const test = () => {
+//   let name = "Abduvohidov";  ///shadowi shu yerda log qisak
+//   ///////////bir hil nom bilan veraybl shadow boladi
+//   console.log(name);
+// };
+// test();
+// console.log(name);
+
+//////////////
+
+// const telegram = (name, surname = "") => {  ///default parametr
+//   // if(surname){
+//   //   console.log(name,surname);
+//   // }else{
+//   //   console.log(name);
+//   // }
+
+//   // surname ? console.log(name,surname):console.log(name); //ternary operation
+//   console.log(name, surname);  // || ""
+// };
+// telegram("Webbrain");
+
+///////////////
+
+// const gpa=()=>{
+//   return "okay?"
+// }
+
+// console.log(gpa());  ///undefined chiqadi
+
+/////////////
+
+// const gpa = (a = 0, b = 0, c = 0, d = 0) => {
+//   let temp = (a + b + c + d) / 4; //orta arifmetigi
+//   console.log(temp);
+//   return "okay?";
+// };
+// // console.log(gpa(4.5, 3, 3.5, 2));//3.25
+// // console.log(gpa(4, 3.5, 4.5, 3));//3.75
+// // console.log(gpa(3.5, 3, 2, 4));//3.124
+// // console.log(gpa(2.5, 3.5, 4, 2));//3
+
+// console.log(
+//   gpa(4.5, 3, 3.5, 2) +
+//     gpa(4, 3.5, 4.5, 3) +
+//     gpa(3.5, 3, 2, 4) +
+//     gpa(2.5, 3.5, 4, 2) / 4
+// );
+
+////////////////
+
+// const getNum = (a) => {
+//   // console.log(a);
+//   // return a;  ///value return yozishimiz kerak valega birlashyapti returnda
+//   let total = 193;
+//   // console.log((a * 100) / total);
+//   return (a * 100) / total; ///return degani verayblga biriktirib qoyilyapti degani
+// };
+
+// console.log(parseInt(getNum(159)));
+
+///////
+
+// const gpa = (a = 0, b = 0, c = 0, d = 0) => {
+//   let temp = (a + b + c + d) / 4; //orta arifmetigi
+//   // console.log(temp);
+//   return temp;
+// };
+
+// console.log(gpa(5, 3, 3, 2));  ///3.25 chiqadi 
+
+
+/////////////get+
+
+// const getGpa = (a = 0, b = 0, c = 0, d = 0) => {
+//   let temp = (a + b + c + d) / 4; //orta arifmetigi
+//   // console.log(temp);
+//   return temp;
+// };
+
+// console.log(getGpa(5, 3, 3, 2));  ///3.25 chiqadi 
