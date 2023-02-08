f5987ed2455e8ed1c3967c74bdad0376390279cf
Mirabbosbek9778 <jwicj969@gmail.com>
2023-02-08 16:05:06 +0500

    String les

---
+++ lesson9/index.js
@@ -0,0 +1,104 @@
+// let str1 = "1   webb \t' rain'     ";
+// let str2 = "2webbrain";
+// let str3 = `3webbrain
+// ${str1}
+// `;
+// // let str4 = str1 + str2;
+
+// console.log(str3);
+
+/////////////////////////////////////////////
+
+// let str1 = "webbrain";/
+// let str2 = new String("webbrai");
+
+// console.log(str1 === "webbrain"); ///true
+// console.log(str2 === "webbrain"); ///fasle
+// console.log(str2); ///[String: 'webbrain']
+
+// console.log(str2.localeCompare(str1)); //0
+
+// console.log(str2 == "webbrain"); ///fasle
+
+// console.log(str1.localeCompare(str2)); //-1
+///str2==str1
+
+////////////////
+
+// let str1 = "webbrain";
+
+// console.log(str1[1]);
+// console.log(str1[-1]);
+// console.log(str1.at(-1));
+// console.log(str1.charAt(1));
+
+//////////////////
+
+// let str1 = "MIRABBOSBEK ";
+// let str1 = 123442;
+
+// console.log(str1.length);
+
+// console.log(`${str1}`.toLowerCase()); ///katta harflar
+// console.log(`${str1}`.toUpperCase()); ///katta harflar
+
+// console.log(str1.toLocaleLowerCase('en-US'));
+// console.log(str1.toLowerCase());
+
+///////////////////////////////
+
+// let str1 = "Mirabbosbek Abduvohidov ";
+
+// console.log(str1.indexOf("Mi",4));
+
+// console.log(str1.lastIndexOf("bb",9));
+
+// let str1 = "Mirabbosbek Abduvohidov ";
+
+// console.log(str1.includes("ov"));///borini chiqaradi
+// console.log(str1.startsWith("Mi"));
+// console.log(str1.endsWith("ov"));
+
+// console.log(str1.padEnd(50, "."));
+// console.log(str1.padStart(50, "."));
+
+/////////////////////////////////////////
+
+// let str1 = "Mirabbosbek Abduvohidov";
+
+// console.log(str1.slice(0,-2));
+
+// console.log(str1.substring(0,2));///kesib oladi
+
+// console.log(str1.substring(2,0));
+
+// console.log(eval(`2+2*2`));  ///harf qatnashsa error
+
+// console.log(str1);
+// console.log(str1.trim());///boshi va ohiridagidani joylarni chopadi
+
+// console.log(str1.split(""));
+
+// console.log(str1.concat("webbrain", "hey"));
+
+// console.log(new String("new ").valueOf());
+// console.log(new String("new "));
+
+// console.log(str1.replace("Abd", "ABD"));
+
+///////////////////////////
+
+// let str = "mirabbos";
+
+// for (char of str) {
+//   console.log(char);
+// }
+
+////////////
+
+// let str1 = "Mirabbosbek";
+
+// console.log(str1.length);
+
+/////
+console.log("hey");
