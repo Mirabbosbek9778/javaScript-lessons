6340de7bd2cc61ade6d39b8c082a7e6c520ac933
Mirabbosbek9778 <jwicj969@gmail.com>
2023-02-24 17:36:58 +0500

    new Map,new Set,Weakmap

---
+++ lesson15/index.js
@@ -0,0 +1,59 @@
+// let obj = {};
+
+// let map = new Map([
+//   ["name", "Mirabbos"],
+//   ["title", "Frontend"],
+// ]);
+
+// console.log(map.title);
+// console.log(Object.fromEntries(map).title);
+
+// let gen = map.entries();
+// gen.next();
+// gen.next();
+// console.log(gen.next());
+
+// console.log(map.keys());
+// console.log(map.values());
+// console.log(map.entries());
+
+// for (let v of map.keys()) {
+//   console.log(v);
+// }
+
+// obj.name = "Abduvohidov";
+// map.title = "Abduvohidov1";
+
+// console.log(obj, obj.name);
+// console.log(map.size, map.get(123));
+// console.log(map.delete(123)); //true
+// console.log(map.has(123)); //true
+
+// map.set(123, "Frontend1");
+// map.set("title", "Frontend2");
+// map.set("name", "Frontend3");
+// console.log(map.clear());
+// console.log(map);
+
+/////////////////////
+
+// set
+
+// let obj = { name: "Mirabbos" };
+// function name(params) {
+//   console.log();
+// }
+// let set = new Set();
+// set.add("eshmat");
+// set.add("toshmat");
+// // set.add(name);
+// console.log(set);
+// // console.log(set.has('toshmat')); ///true
+
+// let map = new Map([
+//   ["name", "Mirabbos"],
+//   ["title", "Frontend"],
+// ]);
+
+// map = null;
+// console.log(map);
