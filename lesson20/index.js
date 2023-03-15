4e3f472edcae1ad3c3020fd634089157a3c3970a
Mirabbosbek <asd@asd.asd>
2023-03-15 19:20:20 +0500

    class,setter,getter,other

---
+++ lesson20/index.js
@@ -0,0 +1,77 @@
+// // class Object {
+// //   getfull() {
+// //     console.log("hello");
+// //   }
+// // }
+
+// // let user1 = new Object();
+// // let user2 = new Object();
+// // let user3 = new Object();
+
+// // console.log(user1.getfull());
+// // // console.log(user2);
+// // // console.log(user3);
+
+// class Obj {
+//   sum = 0;
+//   getname() {
+//     console.log(++this.sum);
+//   }
+//   getsurname(){
+//     console.log('hello world');
+//   }
+//   constructor() {
+//     console.log("hey");
+//   }
+// }
+
+// let user1 = new Obj();
+// let user2 = new Obj();
+// let user3 = new Obj();
+
+// user1.getname();
+// user1.getname();
+// user1.getname();
+// user1.getname();
+
+// // console.log(user1.getname());
+// // console.log(user2);
+// // console.log(user3);
+
+// // (){}
+
+// class Login {
+//   _psw = "12345678";
+//   #password = 1;
+//   getSignUp() {
+//     console.log(this.#password);
+//   }
+// }
+
+// let user = new Login();
+
+// user.getSignUp();
+// user.getSignUp();
+// user.getSignUp();
+// user.getSignUp();
+
+// console.log(user.password);
+
+// class Obj {
+//   name = "Mirabbosbek";
+//   #surname = "Abduvohidov";
+// //   get getName() {
+// //     console.log(this.name);
+// //   }
+//   set surname(a) {
+//     this.#surname = a;
+//   }
+// }
+
+// let sum = new Obj();
+
+// sum.surname = "123456";
+
+// console.log(sum.surname);
+
+// console.log(sum.getName);
